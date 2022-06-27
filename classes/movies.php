883f904ced0b8c1cc31f3cee297fe302ce222724
Onda-dev <manuelaltieri3@gmail.com>
2022-06-27 15:56:57 +0200

    Classe corretta

--- classes/movies.php
+++ classes/movies.php
@@ -1,17 +1,17 @@
 <?php
 
 class Movie {
-    public $image
     public $title;
     public $year;
     public $director;
     public $score;
-    public $genre
-}
+    public $genre;
 
-public function  __construct($_title, $_genre, $_score)
-{
-    $this->title = $_title;
-    $this->genre = $_genre;
-    $this->score = $_score;
+    public function  __construct($_title, $_genre, $_score)
+    {
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->score = $_score;
+    }
+
 }
--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
-require __DIR__ . '/classes/movies.php'
+    require __DIR__ . '/classes/movies.php';
+
+    $movies = [
+        new Movie('Pulp Fiction', 'Quentin Tarantino', 'Crime'),
+        new Movie('Interstellar', 'Christopher Nolar', 'Sci-fi'),
+        new MOvie('Top Gun: Maverick', 'Joseph Kosinski', "Action")
+    ];
+    var_dump($movies);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
