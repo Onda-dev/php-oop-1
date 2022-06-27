bab926c074ddc64fa7e7d0ba5f28dcbcccbdf8c4
Onda-dev <manuelaltieri3@gmail.com>
2022-06-27 15:46:32 +0200

    first commit

---
+++ classes/movies.php
@@ -0,0 +1,17 @@
+<?php
+
+class Movie {
+    public $image
+    public $title;
+    public $year;
+    public $director;
+    public $score;
+    public $genre
+}
+
+public function  __construct($_title, $_genre, $_score)
+{
+    $this->title = $_title;
+    $this->genre = $_genre;
+    $this->score = $_score;
+}
